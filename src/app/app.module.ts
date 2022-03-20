b0de516698616abd69b3a4cf85d9dd4aeba523bf
RoswelFlores <roswelflores@gmail.com>
2022-03-20 14:21:16 -0300

    Componente Formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,11 +9,14 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule,Routes } from '@angular/router';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes =[
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
   {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent}
 ];
 
 @NgModule({
@@ -21,10 +25,13 @@ const routes: Routes =[
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+  
+  cliente: Cliente = new Cliente();
+  titulo:string ="Crear Cliente"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public create():void{
+    console.log("clicked")
+    console.log(this.cliente)
+  }
+
+}
