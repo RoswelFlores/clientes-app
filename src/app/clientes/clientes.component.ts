ebddd34aba6befffc519c2a052f7b8a5a7ce3bd8
RoswelFlores <roswelflores@gmail.com>
2022-03-26 13:24:12 -0300

    Agregando la ruta paginador y suscribiendo

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -3,7 +3,7 @@ import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import swal from 'sweetalert2';
 import { tap } from 'rxjs';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -13,19 +13,28 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService,
+    private activatedRouted : ActivatedRoute) { }
 
   ngOnInit(): void {
-    let page = 0 ;
-    this.clienteService.getClientes(page).pipe(
-      tap(response =>{
-        console.log('clienteComponent: tap 3');
-        (response.content as Cliente[]).forEach(cliente =>{ 
-          console.log(cliente.nombre)
-        });
-      })
+    
+    this.activatedRouted.paramMap.subscribe(params =>{
+      let page: number = + params.get('page');
       
-    ).subscribe(response => this.clientes = response.content as Cliente[]);
+      if(!page){
+        page = 0;
+      }
+
+      this.clienteService.getClientes(page).pipe(
+        tap(response =>{
+          console.log('clienteComponent: tap 3');
+          (response.content as Cliente[]).forEach(cliente =>{ 
+            console.log(cliente.nombre)
+          })
+        })
+      ).subscribe(response => this.clientes = response.content as Cliente[]);
+    }
+    );
   }
   delete(cliente: Cliente):void{
     swal.fire({
