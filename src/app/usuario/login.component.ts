4476c5e80735ed76c467888678c7eb7143937f94
RoswelFlores <roswelflores@gmail.com>
2022-07-02 18:57:22 -0400

    Incio de sesion y datos guardados sessionStore

--- src/app/usuario/login.component.ts
+++ src/app/usuario/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from './usuario';
 import swal from 'sweetalert2';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
@@ -9,11 +13,15 @@ export class LoginComponent implements OnInit {
   public titulo:string='Por favor Sign In!';
   public usuario:Usuario;
     
-  constructor() { 
+  constructor(private authService: AuthService, private router:Router) { 
     this.usuario = new Usuario();
   }
 
   ngOnInit(): void {
+    if(this.authService.isAuthenticated()){
+      swal.fire('Login',`Hola ${this.authService.usuario.username} ya estas autenticado`, 'info');
+      this.router.navigate(['/clientes']);
+    }
   }
 
   login():void{
@@ -22,6 +30,23 @@ export class LoginComponent implements OnInit {
       swal.fire('Error Login','Username o password vacias!','error');
       return;
     }
+    this.authService.login(this.usuario).subscribe(response=>{
+      console.log(response);
+    
+     
+
+      this.authService.guardarUsuario(response.access_token);
+      this.authService.guardarToken(response.access_token);
+  
+      let usuario = this.authService.usuario;
+      this.router.navigate(['/clientes']);
+      swal.fire('Login',`Hola ${usuario.username}, has iniciado sesion con exito!`,'success');
+    },err=>{
+      if(err.status == 400){
+        swal.fire('Error Login','Usuario o clave incorrecta','error');
+      }
+    }
+    );
   }
 
 }
